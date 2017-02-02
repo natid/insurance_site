71d313170803798cee0a850341553410cf7ffefd
Nati <da.netanel@gmail.com>
2017-02-02 21:04:24 +0200

    fixed url

--- insurance_site/insurace_site/urls.py
+++ insurance_site/insurace_site/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name="insurance_crm/home.html")),
     url(r'^insurance_crm/', include('insurance_crm.urls')),
-
 ]
