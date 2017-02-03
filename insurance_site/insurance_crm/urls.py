0fc0e1984a94ecc661ce3454791bbe2c4912087f
Nati <da.netanel@gmail.com>
2017-02-03 11:59:51 +0200

    commit

--- insurance_site/insurance_crm/urls.py
+++ insurance_site/insurance_crm/urls.py
@@ -1,5 +1,7 @@
 from .api import ClientViewSet, AgentViewSet
 from rest_framework.routers import DefaultRouter
+from django.conf.urls import url, include
+import views
 
 
 router = DefaultRouter()
@@ -7,3 +9,5 @@ router.register(r'clients', ClientViewSet)
 router.register(r'agents', AgentViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns.append(url(r'^nir/', views.nir))
--- insurance_site/insurance_crm/views.py
+++ insurance_site/insurance_crm/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def nir(request):
+    return HttpResponse('<h1>Nir!!!</h1>')
