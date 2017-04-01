d9141f85ad6573aca19dbe586de9dea7307dfe5e
Oren <elisha.oren@hpe.com>
2017-04-01 13:27:23 +0300

    Add First version of Fuse site

---
+++ fuse/src/app/index.module.js
@@ -0,0 +1,27 @@
+(function ()
+{
+    'use strict';
+
+    /**
+     * Main module of the Fuse
+     */
+    angular
+        .module('fuse', [
+
+            // Core
+            'app.core',
+
+            // Navigation
+            'app.navigation',
+
+            // Toolbar
+            'app.toolbar',
+
+            // Quick Panel
+            'app.quick-panel',
+
+            // Sample
+            'app.sample',
+            'app.customers'
+        ]);
+})();
