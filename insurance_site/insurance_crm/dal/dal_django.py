9744bbbe15489e3096630753d63f5c5b7277844a
Nati <da.netanel@gmail.com>
2017-04-10 10:33:29 +0300

    FIX

--- insurance_site/insurance_crm/dal/dal_django.py
+++ insurance_site/insurance_crm/dal/dal_django.py
@@ -15,10 +15,10 @@ def add_cellosign_pdf_response(customer, pdf_file):
 def add_mails_to_client(mails_with_attachments, customer_id, insurance_company_domain):
     insurance_company = [x for x in get_insurance_companies() if x.mail.split("@")[1] == insurance_company_domain][0]
     for mail in mails_with_attachments:
-        attachments_for_db = [attachment.encode("base64") for attachment in mail[1]]
+        #attachments_for_db = [attachment.encode("base64") for attachment in mail[1]]
         response_mail = ResponseMail.objects.create(client_id = customer_id, insurance_company = insurance_company, mail = json.dumps(mail[0]))
-        for attachment, filename in attachments_for_db:
-            Attachment.objects.create(response_mail_id=response_mail.id, attachment=attachment, filename=filename)
+        for attachment, filename in mail[1]:
+            Attachment.objects.create(response_mail_id=response_mail.id, attachment=attachment.encode("base64"), filename=filename)
 
 def _get_credential_entry():
     credentials = Credentials.objects.all()
